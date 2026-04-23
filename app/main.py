a0838c0e608e320071359a89def724be3733728d
Yaroslav Vynogradov <vinogradovarik6@gmail.com>
2026-04-23 21:01:14 +0200

    completed py-linter-formatter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": f"{error['code']} {error['text']}",
+        "type": "error" if error["code"].startswith("E") else "warning"
+    }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+def format_single_linter_file(file_path: str, errors: list[dict]) -> dict:
+    return {
+        file_path: [format_linter_error(error) for error in errors]
+    }
 
 
-def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+def format_linter_report(linter_report: dict) -> list[dict]:
+    return [
+        format_linter_error(error)
+        for file_errors in linter_report.values()
+        for error in file_errors
+    ]
